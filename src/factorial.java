bb7d40d143420181e02fc3974a77e51b9e31e3f2
Vishal Shah <vishal12kl@gmail.com>
2025-07-02 01:36:39 +0530

    WIP(dsa): added factorial

---
+++ src/factorial.java
@@ -0,0 +1,15 @@
+public class factorial {
+
+  static int fact(int n) {
+    int f = 1;
+
+    for (int i = 1; i <= n; i++) {
+      f = f * i;
+    }
+    return f;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(fact(7));
+  }
+}
