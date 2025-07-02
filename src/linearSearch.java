92c65397ed59d72cf4fda6755a3eacbb18fe9cb1
Vishal Shah <vishal12kl@gmail.com>
2025-07-02 10:10:20 +0530

    WIP(dsa): linear search in arrays

---
+++ src/linearSearch.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class linearSearch {
+
+  static int linearsearch(int numbers[], int key) {
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] == key) {
+        return numbers[i];
+      }
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    int numbers[] = { 23, 45, 67, 87, 34, 53, 67, 32, 98 };
+    int key = 53;
+
+    int index = linearsearch(numbers, key);
+    System.out.println("key found at index: " + index);
+  }
+}
